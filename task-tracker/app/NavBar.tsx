b891657876ba34249eff332128335bb046db4f28
Klay-Michael Dehaney <kdehaney53@gmail.com>
2024-01-20 19:17:50 -0500

    conditional link styling

--- task-tracker/app/NavBar.tsx
+++ task-tracker/app/NavBar.tsx
@@ -1,8 +1,13 @@
+'use client';
 import React from 'react'
 import Link from 'next/link'
 import { IoBug } from "react-icons/io5";
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const NavBar = () => {
+
+    const currentPath = usePathname();
 
     const links = [
         {label: 'Dashboard', href:'/'},
@@ -16,7 +21,11 @@ const NavBar = () => {
             {links.map(link => 
             <Link 
             key={link.href} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+            className={classnames({
+                'text-zinc-900' : link.href === currentPath,
+                'text-zinc-500' : link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors' : true
+            })}
             href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
