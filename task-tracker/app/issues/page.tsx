f8725865d7172929e3cff2760473f6ff8e505a6e
Klay-Michael Dehaney <kdehaney53@gmail.com>
2024-01-22 16:03:19 -0500

    import radix

--- task-tracker/app/issues/page.tsx
+++ task-tracker/app/issues/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {Button} from '@radix-ui/themes';
 
 const Issuespage = () => {
   return (
-    <div>Issuespage</div>
+    <div><Button>New Issue</Button></div>
   )
 }
 
